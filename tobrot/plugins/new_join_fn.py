e6819b405a1815da6d73fa60d22923fbbd754743
rootgamer9870-glitch <86337179+rootgamer9870-glitch@users.noreply.github.com>
2021-08-12 15:38:56 +0600

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,6 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        """<b> mirror house</i>""",
+        """<b></i>""",
         disable_web_page_preview=True,
     )
