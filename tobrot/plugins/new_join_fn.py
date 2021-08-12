864284200fe75292b396619bd5f76f7ff9e49458
rootgamer9870-glitch <86337179+rootgamer9870-glitch@users.noreply.github.com>
2021-08-12 14:48:52 +0600

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -8,16 +8,6 @@ import pyrogram
 from tobrot import AUTH_CHANNEL, LOGGER
 
 
-async def new_join_f(client, message):
-    chat_type = message.chat.type
-    if chat_type != "private":
-        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
-        # leave chat
-        await client.leave_chat(chat_id=message.chat.id, delete=True)
-    # delete all other messages, except for AUTH_CHANNEL
-    await message.delete(revoke=True)
-
-
 async def help_message_f(client, message):
     # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
     # channel_id = str(AUTH_CHANNEL)[4:]
